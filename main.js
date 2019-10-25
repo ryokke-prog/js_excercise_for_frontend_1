b3ae7ce55e5f1002a36db5fe9c761e894556ac5b
ryokke <ryotahbb@icloud.com>
2019-10-25 22:33:42 +0900

    improve main.js

--- main.js
+++ main.js
@@ -3,31 +3,35 @@
  *   - getElementByIdを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
-
+const titleElement1 = document.getElementById('title');
+console.log('titleElement1の中身：', titleElement1);
 /**
  * 課題2: id値が 'item-container' の要素を取得する
  *   - querySelectorを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const containerElement = document.querySelector('#item-container');
+console.log('containarElementの中身：', containerElement);
 
 /**
  * 課題3: li要素を全て取得する
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const liElements = document.getElementsByTagName('li');
+console.log('liElements：', liElements);
 
 /**
  * 課題4: class値が 'item' の要素を全て取得する
  *   - getElementsByClassNameを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const itemElements1 = document.getElementsByClassName('item');
+console.log('itemElements1：', itemElements1);
 
 /**
  * 課題5: class値が 'item' の要素を全て取得する
  *   - querySelectorAllを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const itemElements2 = document.querySelectorAll('.item');
+console.log('itemElements2：', itemElements2);
